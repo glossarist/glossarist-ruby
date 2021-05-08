0e875bcebd26b10a2faa1a0f3053b2ab5f95e6b4
Sebastian Skałacki <skalee@gmail.com>
2021-05-08 12:19:38 +0200

    wipo

--- lib/glossarist/model.rb
+++ lib/glossarist/model.rb
@@ -4,5 +4,17 @@ module Glossarist
   class Model
     include ActiveModel::Model
     include ActiveModel::Attributes
+
+    def to_h
+      attributes.to_h.transform_values { |v| serialize_attibute_value v }
+    end
+
+    def serialize_attibute_value(attr_value)
+      case attr_value
+      when Model then attr_value.to_h
+      when Array then attr_value.map { |x| serialize_attibute_value(x) }
+      else attr_value
+      end
+    end
   end
 end
