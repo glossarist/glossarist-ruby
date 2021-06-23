5372ed392bd919f4e5a4eb2e38347e3b2759d783
Sebastian Skałacki <skalee@gmail.com>
2021-06-23 02:41:06 +0200

    wip

---
+++ spec/features/attributes_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "Concepts and their attributes" do
+  specify "concepts are translatable, and have various attributes" do
+    concept = Glossarist::Concept.new
+    concept.id = "123-45"
+
+    # TODO dunno
+  end
+end
