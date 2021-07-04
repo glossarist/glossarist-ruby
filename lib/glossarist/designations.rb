864cd2432e09b047bfb0e85eb1203d847575b098
Sebastian Skałacki <skalee@gmail.com>
2021-07-04 16:35:20 +0200

    Define serialization of designation models

--- lib/glossarist/designations.rb
+++ lib/glossarist/designations.rb
@@ -12,6 +12,27 @@ module Glossarist
 
       attr_accessor :normative_status
       attr_accessor :geographical_area
+
+      def self.from_h(hash)
+        type = hash["type"]
+
+        if type.nil? || /\w/ !~ type
+          raise ArgumentError, "designation type is missing"
+        end
+
+        designation_subclass = SERIALIZED_TYPES[type]
+
+        if self == Base
+          # called on Base class, delegate it to proper subclass
+          SERIALIZED_TYPES[type].from_h(hash)
+        else
+          # called on subclass, instantiate object
+          unless SERIALIZED_TYPES[self] == type
+            raise ArgumentError, "unexpected designation type: #{type}"
+          end
+          super(hash.reject { |k, _| k == "type" })
+        end
+      end
     end
 
     class Expression < Base
@@ -20,10 +41,42 @@ module Glossarist
       attr_accessor :plurality
       attr_accessor :prefix
       attr_accessor :usage_info
+
+      def to_h
+        {
+          "type" => "expression",
+          "prefix" => prefix,
+          "normative_status" => normative_status,
+          "usage_info" => usage_info,
+          "designation" => designation,
+          "part_of_speech" => part_of_speech,
+          "geographical_area" => geographical_area,
+          "gender" => gender,
+          "plurality" => plurality,
+        }.compact
+      end
     end
 
     class Symbol < Base
       attr_accessor :international
+
+      def to_h
+        {
+          "type" => "symbol",
+          "normative_status" => normative_status,
+          "geographical_area" => geographical_area,
+          "designation" => designation,
+          "international" => international,
+        }.compact
+      end
     end
+
+    # Bi-directional class-to-string mapping for STI-like serialization.
+    SERIALIZED_TYPES = {
+      Expression => "expression",
+      Symbol => "symbol",
+    }
+    .yield_self { |h| h.merge(h.invert) }
+    .freeze
   end
 end
