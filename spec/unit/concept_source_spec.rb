1981e20a9212d2aaf9412b9822bc4862341770f7
HassanAkbar <hassanakbar2@gmail.com>
2024-12-09 17:49:58 +0500

    fix: updated enum specs for lutaml-model

--- spec/unit/concept_source_spec.rb
+++ spec/unit/concept_source_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Glossarist::LutamlModel::ConceptSource do
-  # it_behaves_like "an Enum"
+  it_behaves_like "an Enum"
 
   subject { described_class.from_yaml(attributes) }
 
